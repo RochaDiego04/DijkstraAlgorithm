330f6cf0eaa6f0c6387e913f1dfad037b5640d25
Diego Rocha <drochaesc@gmail.com>
2023-04-09 17:28:06 -0600

    Adding frames to Dijkstra Visual

--- Dijkstra_Visual.py
+++ Dijkstra_Visual.py
@@ -1,46 +1,39 @@
-from Board import board
-import pygame
-
-pygame.init()
-
-WIDTH = 900
-HEIGHT = 950
-screen  = pygame.display.set_mode([WIDTH, HEIGHT])
-timer = pygame.time.Clock()
-fps = 60
-level = board
-
-# BOARD specifications
-HORIZONTAL = 4
-VERTICAL = 4
-
-font = pygame.font.Font('freesansbold.ttf', 20)
+from tkinter import *
+import settings
+import utilities
 
 
-def draw_board():
-    num1 = ((HEIGHT - 50) // VERTICAL)
-    num2 = (WIDTH // HORIZONTAL)
-    for i in range(len(level)):
-        for j in range(len(level[i])):
-            if level[i][j] == 0:
-                x = (j * num2 + (0.5*num2))
-                y = (i * num1 + (0.5*num1))
-                #screen.blit(monster_sprite,(x, y))
-                pygame.draw.circle(screen, 'white', (j * num2 + (0.5*num2), i * num1 + (0.5*num1)), 4)
-        
+root = Tk()
 
-# Load the sprite for monsters
-monster_sprite = pygame.image.load("img/Monstruo.png")
+# Create Window
+root.configure(bg='black')
+root.title(f"{settings.WIDTH}x{settings.HEIGHT}")
+root.geometry('1440x720')
+root.resizable(False, False)
 
-run = True
-while run:
-    timer.tick(fps)
-    screen.fill('black')
-    draw_board()
+top_frame = Frame(
+    root,
+    bg='red',
+    width=settings.WIDTH,
+    height=utilities.height_percentage(25)
+)
+top_frame.place(x=0,y=0)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    
-    pygame.display.flip()
-pygame.quit()
+left_frame = Frame(
+    root,
+    bg='blue',
+    width=utilities.width_percentage(25),
+    height=utilities.height_percentage(75)
+)
+left_frame.place(x=0, y=utilities.height_percentage(25))
+
+center_frame = Frame(
+    root,
+    bg='black',
+    width=utilities.width_percentage(75),
+    height=utilities.height_percentage(75)
+)
+center_frame.place(x=utilities.width_percentage(25), y=utilities.height_percentage(25))
+
+# Run window
+root.mainloop()
